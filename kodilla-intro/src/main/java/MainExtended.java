d0997c30a6bf9c3f5c858772f271cbbf9804be6d
Roman Avramets <romanavr92@gmail.com>
2023-02-28 13:05:37 +0100

    Task(Zmienne)

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,12 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myFirstName = "Roman";
+        String mySecondName = "Avramets";
+        int myAge = 31;
+        char driverLicense = 'B';
+        boolean isMusicLover = true;
+        boolean isMonthDecember = false;
+        System.out.println(isMonthDecember);
+
+    }
+}
