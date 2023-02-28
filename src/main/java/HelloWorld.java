2ec53395ba7065a44c0e5676a090a0c2ab3546a0
Roman Avramets <romanavr92@gmail.com>
2023-02-28 12:06:03 +0100

    Initial version

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Roman");
+        System.out.println(31);
+        System.out.println('A');
+    }
+}
