bb6d7b4446be488bcd2b05467436dd8e199c0db5
Roman Avramets <romanavr92@gmail.com>
2023-03-01 19:49:13 +0100

    Refactor

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,8 +1,9 @@
-public class Loops {
-    public static void main (String[] args) {
-        for (int i=0; i <= 10; i++) {
+public class Loops{
+    public static void main(String[] args) {
+        int i =0;
+        while (i <= 10) {
             System.out.println(i);
+            i++;
         }
-
     }
 }
