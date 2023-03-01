df8ce790db5080854c2090fdf38d528aded71608
Roman Avramets <romanavr92@gmail.com>
2023-03-01 12:52:41 +0100

    Task

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,14 @@
+public class LeapYear {
+    public static void main (String[] args) {
+        int year = 2022;
+        boolean isLeapYear;
+        isLeapYear = (year % 4 == 0);
+        isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+        }
+
---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,8 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[] {"cat", "dog", "fish", "bird", "monkey"};
+        int numberOfElements = animals.length;
+        System.out.println("Moja tablica zawiera elementow");
+        System.out.println(numberOfElements);
+    }
+}
