4c4ffcc289297af62b35ce951a0684d3c4d7947e
Roman Avramets <romanavr92@gmail.com>
2023-03-14 15:21:43 +0100

    Modul2

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public Book (String author, String title) {
+        this.author;
+        this.title;
+    }
+}
---
+++ kodilla-intro/src/main/java/BookApp.java
@@ -0,0 +1,7 @@
+public class BookApp {
+    public static void main(String[] args) {
+        Book book = new Book ("Isaac Asimow", "The Galaxy");
+        System.out.println(book.author);
+        System.out.println(book.title);
+    }
+}
