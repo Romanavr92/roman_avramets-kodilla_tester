d7eeb2877a9a1db00a0818566638bad6014425bd
Roman Avramets <romanavr92@gmail.com>
2023-03-24 14:31:48 +0100

    4Modul

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -0,0 +1,32 @@
+package com.kodilla.collections.interfaces;
+
+public class InterfacesDemo {
+    public static void main(String[] args) {
+        Square square = new Square(10);
+        showShapeDetails(square);
+        Circle circle = new Circle(7);
+        showShapeDetails(circle);
+        Triangle triangle = new Triangle(4, 3, 5);
+        showShapeDetails(triangle);
+    }
+    private static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
+    }
+
+   /* private static void showSquareDetails(Square square) {
+        System.out.println(square.getArea());
+        System.out.println(square.getPerimeter());
+    }
+
+    private static void showCircleDetails(Circle circle) {
+        System.out.println(circle.getArea());
+        System.out.println(circle.getPerimeter());
+    }
+
+    private static void showTriangleDetails(Triangle triangle) {
+        System.out.println(triangle.getArea());
+        System.out.println(triangle.getPerimeter());
+    }
+*/
+}
